7a88bf8809db335cf9c85c59a43b4876547cbc26
Bird <example@example.com>
2025-11-16 22:03:27 +0000

    add more metadata monitoring

--- lib_land_registry_data/lib_db.py
+++ lib_land_registry_data/lib_db.py
@@ -1,6 +1,7 @@
 
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
+from sqlalchemy import BigInteger
 
 from sqlalchemy.orm import DeclarativeBase
 
@@ -45,17 +46,32 @@ class PPCompleteMetadata(LandRegistryBase):
     __tablename__ = 'pp_complete_metadata'
 
     pp_complete_metadata_id: Mapped[int] = mapped_column(primary_key=True)
-    download_size_bytes: Mapped[int]
+    download_size_MB: Mapped[int] = mapped_column(BigInteger)
     auto_date: Mapped[date]
     process_start_timestamp: Mapped[datetime]
     process_complete_timestamp: Mapped[datetime]
     process_duration: Mapped[timedelta]
-    download_start_timestamp: Mapped[datetime]
-    download_complete_timestamp: Mapped[datetime]
+    #download_start_timestamp: Mapped[datetime]
+    #download_complete_timestamp: Mapped[datetime]
     download_duration: Mapped[timedelta]
-    pandas_read_start_timestamp: Mapped[datetime]
-    pandas_read_complete_timestamp: Mapped[datetime]
+    #pandas_read_start_timestamp: Mapped[datetime]
+    #pandas_read_complete_timestamp: Mapped[datetime]
     pandas_read_duration: Mapped[timedelta]
-    database_upload_start_timestamp: Mapped[datetime]
-    database_upload_complete_timestamp: Mapped[datetime]
+    #pandas_datetime_convert_start_timestamp: Mapped[datetime]
+    #pandas_datetime_convert_complete_timestamp: Mapped[datetime]
+    pandas_datetime_convert_duration: Mapped[timedelta]
+    #pandas_write_start_timestamp: Mapped[datetime]
+    #pandas_write_complete_timestamp: Mapped[datetime]
+    pandas_write_duration: Mapped[timedelta]
+    #database_upload_start_timestamp: Mapped[datetime]
+    #database_upload_complete_timestamp: Mapped[datetime]
     database_upload_duration: Mapped[timedelta]
+
+
+class TestTable(LandRegistryBase):
+
+    __tablename__ = 'test_table'
+
+    test_table_id: Mapped[int] = mapped_column(primary_key=True)
+    string_column: Mapped[str]
+    int_column: Mapped[int]
