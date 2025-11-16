efde7b7e278c15ee31da297b88039b62acae9698
Bird <example@example.com>
2025-11-16 22:03:38 +0000

    upgrade to psycopg3

--- lib_land_registry_data/lib_env.py
+++ lib_land_registry_data/lib_env.py
@@ -23,6 +23,7 @@ class EnvironmentVariables():
         self.postgres_user = postgres_user
         self.postgres_password = postgres_password
         self.postgres_database = postgres_database
+        self.postgres_port = 5432
 
     def get_postgres_connection_string(self) -> str:
         postgres_user = self.postgres_user
@@ -38,6 +39,32 @@ class EnvironmentVariables():
         postgres_host = self.postgres_host
         postgres_database = self.postgres_database
         postgres_connection_string = f'postgresql+psycopg2://{postgres_user}:{postgres_password}@{postgres_host}/{postgres_database}'
+        return postgres_connection_string
+
+    def get_postgres_psycopg3_connection_string(self) -> str:
+        postgres_user = self.postgres_user
+        postgres_password = self.postgres_password
+        postgres_host = self.postgres_host
+        postgres_database = self.postgres_database
+        postgres_connection_string = f'postgresql+psycopg://{postgres_user}:{postgres_password}@{postgres_host}/{postgres_database}'
+        return postgres_connection_string
+
+    def get_postgres_psycopg3_connection_string_with_port(self) -> str:
+        postgres_user = self.postgres_user
+        postgres_password = self.postgres_password
+        postgres_host = self.postgres_host
+        postgres_database = self.postgres_database
+        postgres_port = self.postgres_port
+        postgres_connection_string = f'postgresql+psycopg://{postgres_user}:{postgres_password}@{postgres_host}:{postgres_port}/{postgres_database}'
+        return postgres_connection_string
+
+    def get_psycopg3_postgres_connection_string_as_key_value_pairs(self) -> str:
+        postgres_user = self.postgres_user
+        postgres_password = self.postgres_password
+        postgres_host = self.postgres_host
+        postgres_database = self.postgres_database
+        postgres_port = self.postgres_port
+        postgres_connection_string = f'user={postgres_user} password={postgres_password} host={postgres_host} dbname={postgres_database} port={postgres_port}'
         return postgres_connection_string
 
     def get_postgres_host(self) -> str:
